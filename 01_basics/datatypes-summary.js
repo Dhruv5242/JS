5344965bea61125def6de49c6b945c7e0180a878
Dhruv5242 <90721151+Dhruv5242@users.noreply.github.com>
2024-10-22 16:19:08 +0000

    data type summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,8 @@
+//Primitive
+//7 Types - String, Number, Boolean, null, Undefined, symbol, BigInt
+
+//Refrence(Non-primitive)
+//Array, Objects, Functions
+
+const bigNumber = 3456743123423553n;
+console.log(typeof bigNumber);
